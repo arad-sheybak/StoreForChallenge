c81ca085df6590861069556a5e980b329496b6bd
Arad Sheybak <arad.sheybak@outlook.com>
2021-06-17 17:57:32 +0430

    day 22 --> slider customized

--- app/src/main/java/ir/aradsheybak/storeforchallenge/Fragments/Home/HomeFragment.kt
+++ app/src/main/java/ir/aradsheybak/storeforchallenge/Fragments/Home/HomeFragment.kt
@@ -13,33 +13,39 @@ import ir.aradsheybak.storeforchallenge.databinding.FragmentHomeBinding
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
     private val imageList = ArrayList<SlideModel>()
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+
+        addImageToSlider()
+        setArrayImageToSlider()
+
+        return binding.root
+    }
+//https://github.com/denzcoskun/ImageSlideshow ---> slider repository
+
+    private fun addImageToSlider(){
         imageList.add(
             SlideModel(
-                R.drawable.ic_logo,
-                "The animal population decreased by 58 percent in 42 years."
+                "https://d39l2hkdp2esp1.cloudfront.net/img/photo/147422/147422_00_2x.jpg", ScaleTypes.CENTER_CROP
             )
         )
         imageList.add(
             SlideModel(
-                R.drawable.ic_logo_login,
-                "Elephants and tigers may become extinct."
+                "https://www.gardeningknowhow.com/wp-content/uploads/2019/08/flower-color.jpg", ScaleTypes.CENTER_CROP
             )
         )
         imageList.add(
             SlideModel(
                 "https://www.gardendesign.com/pictures/images/675x529Max/site_3/helianthus-yellow-flower-pixabay_11863.jpg",
-                "And people do that.",
                 ScaleTypes.CENTER_CROP
             )
         )
-        binding.sliderHome.setImageList(imageList)
-        return binding.root
     }
-//https://github.com/denzcoskun/ImageSlideshow ---> slider repository
+
+    private fun setArrayImageToSlider(){
+        binding.sliderHome.setImageList(imageList)
+
+    }
+
+
 }
